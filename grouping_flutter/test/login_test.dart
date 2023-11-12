488976a4a2c629811099a294737a53e30823eca3
Dilamissu <dilamissushen@gmail.com>
2023-11-12 21:23:51 +0800

    Feats: add user_already_exsisted exception

--- grouping_flutter/test/login_test.dart
+++ grouping_flutter/test/login_test.dart
@@ -1,51 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:grouping_project/exceptions/auth_service_exceptions.dart';
+import 'package:grouping_project/service/auth/account.dart';
+
 void main() {
-  // group('account login check', () {
-  //   WidgetsFlutterBinding.ensureInitialized();
-  //   AuthService authService = AuthService();
-  //   FlutterSecureStorage storage = FlutterSecureStorage();
-  //   test('signUp', () async {
-  //     authService
-  //         .signUp(account: 'test', password: 'testtest')
-  //         .whenComplete(() {
-  //       storage
-  //           .containsKey(key: 'auth-token')
-  //           .then((value) => expect(value, true));
-  //     }).onError((error, stackTrace) {
-  //       error as AuthServiceException;
-  //       expect(error.code == 'user_already_exist', true);
-  //       log(error.toString());
-  //     });
-  //   });
+  group('account login check', () {
+    AccountAuth accountAuth = AccountAuth();
+    FlutterSecureStorage storage = FlutterSecureStorage();
+    WidgetsFlutterBinding.ensureInitialized();
+    test('signUp', () async {
+      accountAuth
+          .signUp(account: 'test', password: 'testtest', username: 'testestest')
+          .whenComplete(() {
+        storage
+            .containsKey(key: 'auth-token')
+            .then((value) => expect(value, true));
+      }).onError((error, stackTrace) {
+        error as AuthServiceException;
+        expect(error.code == 'user_already_exist', true);
+      });
+    });
 
-  //   test('logout', () async {
-  //     authService.logOut().whenComplete(() {
-  //       storage
-  //           .containsKey(key: 'auth-token')
-  //           .then((value) => expect(value, false));
-  //     });
-  //   });
+    test('logout', () async {
+      accountAuth.logOut().whenComplete(() {
+        storage
+            .containsKey(key: 'auth-token')
+            .then((value) => expect(value, false));
+      });
+    });
 
-  //   test('wrong password', () async {
-  //     authService
-  //         .signIn(account: 'test', password: '878787')
-  //         .onError((error, stackTrace) {
-  //       error as AuthServiceException;
-  //       expect(error.code == 'wrong_password', true);
-  //     });
-  //   });
+    test('wrong password', () async {
+      accountAuth
+          .signIn(account: 'test', password: '878787')
+          .onError((error, stackTrace) {
+        error as AuthServiceException;
+        expect(error.code == 'wrong_password', true);
+      });
+    });
 
-  //   test('sign in', () async {
-  //     authService
-  //         .signIn(account: 'test', password: 'testtest')
-  //         .whenComplete(() {
-  //       storage
-  //           .containsKey(key: 'auth-token')
-  //           .then((value) => expect(value, true));
-  //     });
-  //   });
-  // });
-  // group('Google login test', () {
-  //   AuthService authService = AuthService();
-  //   authService.googleSignIn().then((value) => Placeholder());
-  // });
+    test('sign in', () async {
+      accountAuth
+          .signIn(account: 'test', password: 'testtest')
+          .whenComplete(() {
+        storage
+            .containsKey(key: 'auth-token')
+            .then((value) => expect(value, true));
+      });
+    });
+  });
 }
