aca416768e14d264c21734d47035cf5299cd3ad0
Dilamissu <dilamissushen@gmail.com>
2023-11-25 00:10:16 +0800

    refactor: remove material that is with foundation

--- grouping_flutter/lib/auth/presentation/view_models/login_view_model.dart
+++ grouping_flutter/lib/auth/presentation/view_models/login_view_model.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:grouping_project/app/presentation/providers/message_service.dart';
 import 'package:grouping_project/auth/data/datasources/auth_local_data_source.dart';
