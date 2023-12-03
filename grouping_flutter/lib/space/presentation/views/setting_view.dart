0ba98b0ee835ed9029d6271478b861ac54e391b0
Dilamissu <dilamissushen@gmail.com>
2023-12-03 18:42:05 +0800

    refactor: remove extra codes

--- grouping_flutter/lib/space/presentation/views/setting_view.dart
+++ grouping_flutter/lib/space/presentation/views/setting_view.dart
@@ -1,14 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:grouping_project/core/shared/color_widget_interface.dart';
 import 'package:grouping_project/space/presentation/view_models/workspace_view_model.dart';
-import 'package:grouping_project/space/presentation/views/components/color_card_with_fillings.dart';
-
-/*
-cd grouping_flutter
-flutter run --web-port 5000
-2
-
-*/
 
 class SettingView extends StatefulWidget {
   const SettingView({super.key, required this.viewModel});
@@ -23,6 +14,9 @@ class _SettingViewState extends State<SettingView> {
   @override
   Widget build(BuildContext context) {
     // WorkspaceViewModel viewModel = widget.viewModel;
-    return const Placeholder();
+    return const Center(
+      child: Text('building setting page...'),
+    );
+    ;
   }
 }
