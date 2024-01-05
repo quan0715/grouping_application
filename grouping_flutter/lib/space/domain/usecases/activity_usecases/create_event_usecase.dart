6dca966be0ee34749901e8ba4de1e6e3c80c4560
SG <97837305+S-Garry@users.noreply.github.com>
2024-01-05 22:30:18 +0800

    feats: 刪除無用的 code

--- grouping_flutter/lib/space/domain/usecases/activity_usecases/create_event_usecase.dart
+++ grouping_flutter/lib/space/domain/usecases/activity_usecases/create_event_usecase.dart
@@ -12,56 +12,5 @@ class CreateEventUseCase{
 
   Future<Either<Failure, EventEntity>> call(EventEntity entity) async {
     return await _activityRepository.createEvent(entity);
-    // final failureOrEvent = await _activityRepository.createEvent(entity);
-
-    /*
-    return failureOrEvent.fold((failure) {
-      return Left(failure);
-    }, (event) async {
-      // Workspace
-      final failureOrWorkspace =
-          await _workspaceRepository.getWorkspace(event.belongWorkspaceID);
-
-      failureOrWorkspace.fold((failure) {
-        return Left(failure);
-      }, (workspace) => event.belongWorkspace = workspace);
-
-      // Creator
-      final failureOrCreator = await _userRepository.getUser(event.creatorID);
-
-      failureOrCreator.fold((failure) {
-        return Left(failure);
-      }, (creator) => event.creator = creator);
-
-      // Contributors
-      event.contributorIDs.map((contributorID) async {
-        final failureOrContributor =
-            await _userRepository.getUser(contributorID);
-
-        failureOrContributor.fold((failure) {
-          return Left(failure);
-        }, (contributor) {
-          event.contributors.add(contributor);
-        });
-      });
-
-      // Child Missions
-      event.childMissionIDs.map((childMissionID) async {
-        final failureOrChild =
-            await _activityRepository.getMission(childMissionID);
-
-        failureOrChild.fold((failure) {
-          return Left(failure);
-        }, (childMission) {
-          event.childMissions.add(childMission);
-        });
-      });
-
-      // Parent Missions
-      event.parentMissions = [];
-
-      return Right(event);
-    });
-    */
   } 
 }
